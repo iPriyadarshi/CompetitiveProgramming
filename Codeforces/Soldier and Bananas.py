6297718a075e0f25d8321b5a5c23bfd04e714649
iPriyadarshi <priyadarshi1505@gmail.com>
2025-09-13 21:33:33 +0530

    Add solution for Codeforces Problem 546A - Soldier and Bananas

---
+++ Codeforces/Soldier and Bananas.py
@@ -0,0 +1,6 @@
+# Codeforces Problem 546A - Soldier and Bananas
+k, n, w = map(int, input().split())
+
+cost_of_w_bananas = ((w * (w + 1)) / 2) * k
+need_to_borrow = max(0, cost_of_w_bananas - n)
+print(need_to_borrow)
